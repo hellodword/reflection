40b192db9d0650feebec45d8b7b5ea1410cd61ac
hellodword <46193371+hellodword@users.noreply.github.com>
2025-12-26 11:10:17 +0000

    refactor: remove unused

--- reflection-app/src/components/mod.rs
+++
@@ -1,9 +0,0 @@
-mod avatar;
-mod indicator_bin;
-mod multiline_entry;
-mod zoom_level_selector;
-
-pub use self::avatar::Avatar;
-pub use self::indicator_bin::IndicatorBin;
-pub use self::multiline_entry::MultilineEntry;
-pub use self::zoom_level_selector::ZoomLevelSelector;
